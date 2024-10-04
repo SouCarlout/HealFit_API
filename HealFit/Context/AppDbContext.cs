aa9829b7f6f6c42dd68e8b2655923953f90ca75f
JhonatanTana <jhonatan.tana@alunos.unis.edu.br>
2024-10-03 23:48:11 -0300

    Initial commit

---
+++ HealFit/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HealFit.Context {
+    public class AppDbContext : DbContext {  // Alterar de IdentityDbContext para DbContext
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+        }
+
+        //public DbSet<Cliente>? Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            
+
+            base.OnModelCreating(modelBuilder); // Chamar apenas uma vez
+        }
+    }
+}
---
+++ HealFit/Program.cs
@@ -0,0 +1,59 @@
+using HealFit.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+   options.AddDefaultPolicy(
+   policy => {
+       policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+   })
+);
+
+// Configurando a cultura padrão para pt-BR
+var cultureInfo = new CultureInfo("pt-BR");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+// Conexao Banco de Dados
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection,
+    ServerVersion.AutoDetect(mySqlConnection)));
+
+//Repositorios
+/*builder.Services.AddScoped<IUsuarioRepository, USuarioRepository>();*/
+
+
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
